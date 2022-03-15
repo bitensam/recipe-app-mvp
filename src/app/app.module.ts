f2cbb23e32fc1d935e5c0749049ec0dc3a5db057
Kamil Wojkowski <kamil.wojkowski@ppuc.pl>
2022-03-15 12:57:35 +0100

    feat: remove hello-world component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
-  declarations: [AppComponent, HelloWorldComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/hello-world/hello-world.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-hello-world',
-  template: 'Angular v13',
-})
-export class HelloWorldComponent {}
