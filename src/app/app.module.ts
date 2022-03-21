8a970549f81d8e181ab995986b0e6ccfb4300c67
Kamil Wojkowski <kamil.wojkowski@ppuc.pl>
2022-03-21 12:40:34 +0100

    feat: add recipe name subject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,7 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RecipeListComponent,
-    UppercaseFirstLetterPipe,
-    MainComponent,
-    RecipeTileComponent,
-    RatingPipe,
-    RecipeDetailsComponent,
-  ],
+  declarations: [AppComponent, RecipeListComponent, UppercaseFirstLetterPipe, MainComponent, RecipeTileComponent, RatingPipe, RecipeDetailsComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/recipe-tile/recipe-tile.component.ts
+++ src/app/recipe-tile/recipe-tile.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { RecipeService } from '../recipe-details/recipe.service';
 import { Recipe } from '../recipe-list/recipe-list.component';
 
 @Component({
@@ -8,4 +9,10 @@ import { Recipe } from '../recipe-list/recipe-list.component';
 })
 export class RecipeTileComponent {
   @Input() public recipe!: Recipe;
+
+  constructor(private recipeService: RecipeService) {}
+
+  select() {
+    this.recipeService.selectedRecipeName.next(this.recipe.name);
+  }
 }
